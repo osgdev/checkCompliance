c2d30a57200e2d1b99e7c2d9a0ff63dbaa7b1e2f
Pete <swanseatechsupport@hotmail.co.uk>
2018-03-19 10:59:05 +0000

    File type changed.
    AppConfig file changed from YAML to Properties.

--- src/uk/gov/dvla/osg/checkCompliance/AppConfig.java
+++ src/uk/gov/dvla/osg/checkCompliance/AppConfig.java
@@ -1,5 +1,9 @@
 package uk.gov.dvla.osg.checkCompliance;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class AppConfig {
 	
 	private String documentReference, lookupReferenceFieldName, languageFieldName, stationeryFieldName,
@@ -14,9 +18,67 @@ public class AppConfig {
 
 	private String lookupFile, presentationPriorityConfigPath, presentationPriorityFileSuffix, productionConfigPath,
 			productionFileSuffix, postageConfigPath, postageFileSuffix, insertLookup, envelopeLookup, stationeryLookup,
-			papersizeLookup, tenDigitJobIdIncrementValue, mailmarkCompliancePath;
-
+			papersizeLookup, mailmarkCompliancePath;
+	private int tenDigitJobIdIncrementValue;
 	private String presentationPriorityField;
+	
+	public AppConfig(String fileName) throws Exception {
+		Properties prop = new Properties();
+		InputStream input = new FileInputStream(fileName);
+		prop.load(input);
+		
+		documentReference = prop.getProperty("documentReference");
+		lookupReferenceFieldName = prop.getProperty("lookupReferenceFieldName");
+		languageFieldName = prop.getProperty("languageFieldName");
+		stationeryFieldName = prop.getProperty("stationeryFieldName");
+		batchTypeFieldName = prop.getProperty("batchTypeFieldName");
+		subBatchTypeFieldName = prop.getProperty("subBatchTypeFieldName");
+		fleetNoFieldName = prop.getProperty("fleetNoFieldName");
+		groupIdFieldName = prop.getProperty("groupIdFieldName");
+		paperSizeFieldName = prop.getProperty("paperSizeFieldName");
+		mscFieldName = prop.getProperty("mscFieldName");
+		sortField = prop.getProperty("sortField");
+		noOfPagesField = prop.getProperty("noOfPagesField");
+		name1Field = prop.getProperty("name1Field");
+		name2Field = prop.getProperty("name2Field");
+		address1Field = prop.getProperty("address1Field");
+		address2Field = prop.getProperty("address2Field");
+		address3Field = prop.getProperty("address3Field");
+		address4Field = prop.getProperty("address4Field");
+		address5Field = prop.getProperty("address5Field");
+		postCodeField = prop.getProperty("postCodeField");
+		dpsField = prop.getProperty("dpsField");
+		insertField = prop.getProperty("insertField");
+		appNameField = prop.getProperty("appNameField");
+		weightAndSizeField = prop.getProperty("weightAndSizeField");
+		siteFieldName = prop.getProperty("siteFieldName");
+		eightDigitJobIdFieldName = prop.getProperty("eightDigitJobIdFieldName");
+		tenDigitJobIdFieldName = prop.getProperty("tenDigitJobIdFieldName");
+		mailMarkBarcodeContent = prop.getProperty("mailMarkBarcodeContent");
+		eogField = prop.getProperty("eogField");
+		eotField = prop.getProperty("eotField");
+		childSequence = prop.getProperty("childSequence");
+		outerEnvelope = prop.getProperty("outerEnvelope");
+		mailingProduct = prop.getProperty("mailingProduct");
+		insertHopperCodeField = prop.getProperty("insertHopperCodeField");
+		mailMarkBarcodeCustomerContent = prop.getProperty("mailMarkBarcodeCustomerContent");
+		totalNumberOfPagesInGroupField = prop.getProperty("totalNumberOfPagesInGroupField");
+		lookupFile = prop.getProperty("lookupFile");
+		presentationPriorityConfigPath = prop.getProperty("presentationPriorityConfigPath");
+		presentationPriorityFileSuffix = prop.getProperty("presentationPriorityFileSuffix");
+		productionConfigPath = prop.getProperty("productionConfigPath");
+		productionFileSuffix = prop.getProperty("productionFileSuffix");
+		postageConfigPath = prop.getProperty("postageConfigPath");
+		postageFileSuffix = prop.getProperty("postageFileSuffix");
+		insertLookup = prop.getProperty("insertLookup");
+		envelopeLookup = prop.getProperty("envelopeLookup");
+		stationeryLookup = prop.getProperty("stationeryLookup");
+		papersizeLookup = prop.getProperty("papersizeLookup");
+		mailmarkCompliancePath = prop.getProperty("mailmarkCompliancePath");
+		tenDigitJobIdIncrementValue = Integer.valueOf(prop.getProperty("tenDigitJobIdIncrementValue"));
+		presentationPriorityField = prop.getProperty("presentationPriorityField");
+
+	}
 	
 	public String getweightAndSizeField() {
 		return weightAndSizeField;
@@ -206,7 +268,7 @@ public class AppConfig {
 		return papersizeLookup;
 	}
 
-	public String getTenDigitJobIdIncrementValue() {
+	public int getTenDigitJobIdIncrementValue() {
 		return tenDigitJobIdIncrementValue;
 	}
 
