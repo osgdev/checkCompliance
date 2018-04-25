6644a3f883576f09d740093a506eb9aeda2149da
Pete <swanseatechsupport@hotmail.co.uk>
2018-04-25 12:08:50 +0100

    no change

--- src/uk/gov/dvla/osg/checkCompliance/ActualMailProduct.java
+++ src/uk/gov/dvla/osg/checkCompliance/ActualMailProduct.java
@@ -6,6 +6,7 @@ import static uk.gov.dvla.osg.common.classes.Language.*;
 
 import java.util.ArrayList;
 
+import uk.gov.dvla.osg.common.classes.BatchType;
 import uk.gov.dvla.osg.common.classes.Customer;
 import uk.gov.dvla.osg.common.classes.Product;
 import uk.gov.dvla.osg.common.config.PostageConfiguration;
@@ -36,7 +37,7 @@ public class ActualMailProduct {
 				customer.setEnvelope(productionConfig.getEnvelopeWelshUnsorted());
 			}
 			//CHANGE BATCH TYPE TO UNSORTED FOR ALL SORTED
-			if (customer.getBatchType().equals(SORTED)) {
+			if (BatchType.UNSORTED.equals(customer.getBatchType())) {
 				customer.updateBatchType(UNSORTED, PresentationConfiguration.getInstance().lookupRunOrder(UNSORTED));
 			}
 			customer.setProduct(actualMailProduct);
